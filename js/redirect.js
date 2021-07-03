1a3974a162fa4b09bbfd204567dce182d698a351
Alvaro Torres Cogollo <atorrescogollo@gmail.com>
2021-07-03 17:21:59 +0200

    Deprecate this site

---
+++ js/redirect.js
@@ -0,0 +1,8 @@
+var dst="";
+if (window.location.href.indexOf("offensive-tor-toolkit") > -1) {
+	dst = "https://atorrescogollo.gitbook.io/offensive-tor-toolkit/";
+}
+else {
+	dst = "https://atorrescogollo.github.io/";
+}
+window.location.replace(dst);
